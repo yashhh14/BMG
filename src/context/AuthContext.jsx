0376b714708fa47e3ea1d2f05d7087a884662a1b
yashhhh14 <yashwanthsunny4362@gmail.com>
2026-09-25 19:04:05 +0530

    debugging for an api

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,7 +7,7 @@ export const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
     const checkAuth = async () => {
         try {
-            const response = await axios.get(`${API_URL}api/me`,
+            const response = await axios.get(`${API_URL}/api/me`,
                 {
                     withCredentials: true
                 });
